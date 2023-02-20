168b84327826598c864e3697253a085c1535c22c
Rodrigo <rodrigo.bonilla@factorlibre.com>
2023-02-20 11:54:39 +0100

    [FIX] purchase_stock_picking_invoice_link: Se corrige error al cantrolar el tipo de factura ya que in incluiria out_invoice, tambien se descarta los movimientos que ya esten asiciados a una factura.

--- purchase_stock_picking_invoice_link/models/account_invoice.py
+++ purchase_stock_picking_invoice_link/models/account_invoice.py
@@ -20,23 +20,26 @@ class AccountInvoice(models.Model):
     @api.onchange('invoice_line_ids')
     def onchange_invoice_lines_get_po_moves(self):
         pickings = self.env["stock.picking"]
-        if "in" in self.type:
+        if "in_" in self.type:
             for line in self.invoice_line_ids:
-                if line.purchase_line_id:
+                if line.purchase_line_id and not line.move_line_ids:
                     moves = self._get_moves_from_po_line(line.purchase_line_id.id)
+                    moves = moves.filtered(lambda x: not x.invoice_line_ids)
                     line.move_line_ids = [(6, 0, moves.ids)]
                     pickings |= moves.mapped("picking_id")
-            self.picking_ids = [(6, 0, pickings.ids)]
+            if pickings:
+                self.picking_ids = [(6, 0, pickings.ids)]
 
     @api.model
     def create(self, values):
         pickings = self.env['stock.picking']
-        if 'invoice_line_ids' not in values or "in" not in values.get("type", ""):
+        if 'invoice_line_ids' not in values or "in_" not in values.get("type", ""):
             return super().create(values)
         for item in values.get('invoice_line_ids'):
             if not item[2] or not item[2].get('purchase_line_id'):
                 continue
             moves = self._get_moves_from_po_line(item[2]['purchase_line_id'])
+            moves = moves.filtered(lambda x: not x.invoice_line_ids)
             item[2]['move_line_ids'] = [(6, 0, moves.ids)]
             pickings |= moves.mapped('picking_id')
         values['picking_ids'] = [(6, 0, pickings.ids)]
